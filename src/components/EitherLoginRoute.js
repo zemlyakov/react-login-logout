02040f3bde3ec32827cd481a19a137af353223b4
zemlyakov <zemliakov@protonmail.com>
2021-03-13 11:55:34 +0400

    Set up redirects on Login and Logout pages

--- src/components/EitherLoginRoute.js
+++ src/components/EitherLoginRoute.js
@@ -1,7 +1,7 @@
 import { isLoggedIn } from "../services/auth";
 
-const EitherLoginRoute = ({children}) => {
-    return isLoggedIn() ? children[0] : children[1];
-};
+const EitherLoginRoute = ({children}) =>
+    isLoggedIn() ? children[0] : children[1]
+;
 
 export default EitherLoginRoute;
