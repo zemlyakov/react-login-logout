2930b0370bd9c8ce2b3c659fae93040002a46f01
zemlyakov <zemliakov@protonmail.com>
2021-03-15 09:05:53 +0400

    Redirect from root page to login/logout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+    BrowserRouter as Router,
+    Switch,
+    Route, Redirect
 } from "react-router-dom";
 import './App.css';
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import NotFound from "./pages/NotFound";
+import { isLoggedIn } from "./services/auth";
 
 function App() {
   return (
       <Router>
         <Switch>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/logout">
-            <Logout/>
-          </Route>
+            <Route exact path="/">
+                <Redirect to={isLoggedIn() ? '/logout' : '/login'}/>
+            </Route>
+            <Route path="/login">
+                <Login/>
+            </Route>
+            <Route path="/logout">
+                <Logout/>
+            </Route>
             <Route path="*">
-                <NotFound />
+                <NotFound/>
             </Route>
         </Switch>
       </Router>
